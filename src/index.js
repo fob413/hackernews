f28104f94cee54615ee9b066f76e4bec9181efeb
fob1493 <fob1493@gmail.com>
2020-01-23 15:36:52 +0100

    re-render when store is updated

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ import { getReadableStories } from './selectors/story';
 import {STORY_ARCHIVE } from './constants/actionTypes';
 import * as serviceWorker from './serviceWorker';
 
+function render() {
+    ReactDOM.render(
+        <App
+            stories={getReadableStories(store.getState())}
+            onArchive={id => store.dispatch({type: STORY_ARCHIVE, id})}
+        />,
+        document.getElementById('root')
+    );
+}
 
-ReactDOM.render(
-    <App
-        stories={getReadableStories(store.getState())}
-        onArchive={id => store.dispatch({type: STORY_ARCHIVE, id})}
-    />,
-    document.getElementById('root')
-);
-
+store.subscribe(render);
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
